b5e474829dec574096c84d8fd85173b403a21086
MoeAlAnnan <5653@holbertonstudents.com>
2022-09-30 02:32:01 -0700

    function push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
-int main() {
-	puts("\"Programming is like building a multilingual puzzle /n");
-retun 0; }	
+int main()
+
+         { 
+	    puts("\"Programming is like building a multilingual puzzle /n");
+
+	    return 0;
+	 }
+

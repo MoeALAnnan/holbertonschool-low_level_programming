0eb25a002767cd2b48a7af2acf620610fe119532
MoeAlAnnan <5653@holbertonstudents.com>
2022-09-30 04:40:53 -0700

    format specifier

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("size of an int:%zu byte(s) \n", sizeof(int));
 printf("size of a long int:%zu byte(s) \n", sizeof(long int));
 printf("Size of a long long int:%zu byte(s)\n",sizeof(long long int));
 printf("size of a float:%zu byte(s) \n", sizeof(float));
-return (0):
+return (0);
 }

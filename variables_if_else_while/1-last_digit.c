d6530ccb74491d96ef2a47d952cae460f23f29db
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-03 09:28:07 -0700

    extracting last digit and using logic operators error fix7

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int n;
 int x;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-x = n%10;
+x = n %10;
 if (x > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, x);
@@ -22,7 +22,7 @@ else if (x == 0)
 {
 printf("Last digit of %d is %d and is %d\n", n, x, x);
 }
-else if (x < 6 && x!= 0)
+else if (x < 6 && x != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 }

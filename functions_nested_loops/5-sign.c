9e6296d96a0a597d90448a45021e015c3f6a288d
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-06 08:11:31 -0700

    print a sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of an integer.
+ * Description : this program is a checker
+ * @n: int
+ * Return: 1 if > 0, return 0 if = 0, return - if <0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (0 - 5);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (0 - 3);
+}
+}

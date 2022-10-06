2d2ce497214738104bc46ebabf6a77101408825c
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-06 12:01:26 -0700

    function that prints the last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - check if lowercase.
+ * Description : this program is a checker
+ * @i: int
+ * Return: 1 success
+ */
+int print_last_digit(int c)
+{if (c<0)
+{
+c =-1*c %10;
+}
+else
+{
+c =c %10;
+}
+_putchar('0'+ c);
+return (c);
+}
+int main(void)
+{ print_last_digit(-1834);
+return 0;
+}

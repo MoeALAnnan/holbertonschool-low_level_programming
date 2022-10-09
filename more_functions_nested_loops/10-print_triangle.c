761192e05a2ce60f7ba3a2a0e4a6630ed91f87b9
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-09 07:03:51 -0700

    a function that prints triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - pattern function
+ * @size: parameter
+ * Description: a function that prints a triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+
+
+	int j = 0;
+
+	while (j < size)
+	{
+		int i = 1;
+
+		while (i <= size)
+		{
+			if (i < size - j)
+
+				_putchar(' ');
+			else
+				_putchar('0' - 13);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}

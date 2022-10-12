b9b748698fc74b6ca21497fe38895317cb460ed8
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-12 01:10:56 -0700

    a function that prints every other charachter

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ * @s: parameter
+ * Description: a function that prints every other charachter
+ * Return: void
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i = i + 2;
+	}
+	putchar('\n');
+}

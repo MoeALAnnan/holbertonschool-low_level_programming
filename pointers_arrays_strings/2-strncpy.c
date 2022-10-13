7a35de0c20a96b5cc91ed412a7528e3da919cc4e
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-13 05:43:20 -0700

    a function that copy the 1st n divisions error fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,18 +5,41 @@
  * @src: parameter
  * @n: parameter
  * Description: a function that copy
- * Return: (void)
+ * Return: void
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 
 	int i = 0;
+	int j = 0;
+	int x = 0;
 
-	while (i < n)
+	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; src[j] != '\0'; j++)
+	
+	if (n > i)
 	{
-		dest[i] = src[i];
-		i++;
-
+		for (x = 0; x < i; x++)
+			dest[x] = src[x];
 	}
+	else if ( i < n )
+	{
+		for (x = 0; x < n; x++)
+		{
+		dest[x] = src[x];
+		}
+		dest[n] = '\0';
+	}
+
+	else
+	{
+		for (x = 0; x < n; x++)
+		{dest[x] = src[x];}
+
+		
+	}
+
+	
+	
 	return (dest);
 }

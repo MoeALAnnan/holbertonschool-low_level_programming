9501b8af8e626ca456c2b0e3255d8c6a6cb2dc12
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-14 03:03:17 -0700

    a string that capitalizes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - pointer to a string
+ * @c: parameter of the string
+ * Description: a function that capitalize
+ * Return: pointer
+ */
+char *cap_string(char *c)
+{
+	int n = 0;
+	int i;
+
+	while (c[n] != '\0')
+		n++;
+
+	for (i = 0; i < n && c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+
+			if (c[i - 1] == ',' || c[i - 1] == ';'
+					|| c[i - 1] == '.' || c[i - 1] == '!'
+					|| c[i - 1] == '?' || c[i - 1] == '"'
+					|| c[i - 1] == '(' || c[i - 1] == ')'
+					|| c[i - 1] == '{' || c[i - 1] == '}'
+					|| c[i - 1] == '\n' || c[i - 1] == ' ' || c[i - 1] == '\t')
+
+				c[i] = c[i] - 32;
+	}
+
+	return (c);
+}

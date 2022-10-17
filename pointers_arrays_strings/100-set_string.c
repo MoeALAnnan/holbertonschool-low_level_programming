41e8b97fdb5c18fd9f01add3a5db0f8d4fd52d41
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-17 04:56:18 -0700

    a function that adds the sum of diagonals in square matrix

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - a function
+ * @a: parameter
+ * @size: parameter
+ * Description: a function that adds diagonals of square matrix
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum;
+	int sum2 = 0;
+	int j = size - 1;
+
+	sum = 0;
+	sum2 = 0;
+
+	for (i = 0; i < (size * size);)
+	{
+		sum = sum + a[i];
+		i = i + (size + 1);
+	}
+	printf("%d, ", sum);
+
+	for (; j <= (size * size) - size ;)
+	{
+
+		sum2 = sum2 + a[j];
+		j = j + (size - 1);
+	}
+	printf("%d \n", sum2);
+
+}
+

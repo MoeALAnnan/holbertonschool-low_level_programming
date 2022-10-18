395b3d9c051fd3aef38cfd86dc8c98cba18e2bf3
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-18 00:52:05 -0700

    a function that prints

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,17 @@
 #include "main.h"
-
+/**
+ * _puts_recursion - a function
+ * @s: parameter
+ * Description: a function that prints
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		  return;
-	  putchar(*s);
-	    _puts_recursion(s+1);
-	      putchar('\n');
-	        }
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}

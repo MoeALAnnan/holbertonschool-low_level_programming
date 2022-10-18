db2408161b6729b10b60efe246f1896038298aae
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-18 12:55:37 -0700

    a program that multiplies 2 integers from the command line

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argv: argument vector
+ * @argc: argument counter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int result, var1, var2;
+
+	if (argc == 3)
+
+	{var1 = atoi(argv[1]);
+	var2 = atoi(argv[2]);
+
+	result = var1 * var2;
+	printf("%d\n", result);
+	}
+
+	else if (argc == 2)
+		printf("Error\n");
+
+	return (1);
+}

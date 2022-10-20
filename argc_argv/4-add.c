022bebcee2621dd7a40f83fd14a8b36f40b0b30c
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-20 07:21:49 -0700

    a function that adds from the command line

---
+++ argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argv: argument vector
+ * @argc: argument counter
+ * Return: integers
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	/*check if argv exist*/
+
+	if (argc > 1)
+
+	{
+		for (i = 1; i < argc; i++)
+		{
+			/*check if integer or not*/
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+
+				{
+					printf("error\n");
+					return (1);
+				}
+
+
+			}
+
+			sum = sum + atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+
+
+	}
+	else
+		printf("0\n");
+	return (0);
+}

fc5e93c360cfecd2b18f30680ce7ac581e7b101b
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-26 10:20:49 -0700

    a function that allocates memory and initializes values

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * _calloc - a function
+ * @nmemb: parameter
+ * @size: parameter
+ * Description: a function that returns a pointer to a 2 dimensional array
+ * Return: pointer to a pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int x;
+	unsigned int i;
+
+	x = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(x);
+	if (ptr == NULL)
+
+	{
+		free(ptr);
+		ptr = NULL;
+	}
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+	return (ptr);
+
+}
+

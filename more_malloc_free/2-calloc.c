f84d590cb814a8e47dc80674bd89dd424ab4f905
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-26 10:48:03 -0700

    a function that allocates memory and initializes values error fix 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -26,11 +26,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		free(ptr);
 		ptr = NULL;
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+		ptr[i] = '\0';
 	return (ptr);
 
 }

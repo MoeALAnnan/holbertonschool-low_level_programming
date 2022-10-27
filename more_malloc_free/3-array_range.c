0dcf6fd6a420810932dfe2ac6419a76330b69a0c
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-27 02:31:24 -0700

    a function that arranges an array error fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,12 +1,13 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
 /**
- * array_arrange - a function
+ * array_range - a function
  * @min: parameter
  * @max: parameter
- * Description: a function that arranges an array of intgeres
+ * Description: a function that stores a range of intgeres
  * Return: pointer
  */
 int *array_range(int min, int max)

794c2d3930af413f4e4dd930256da3acc41f5bfb
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-31 04:16:14 -0700

    defining a new data type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure data type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: pet identifier
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif

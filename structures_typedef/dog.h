d011478b65a9363f28b85a4a214a588c7777a874
MoeAlAnnan <5653@holbertonstudents.com>
2022-10-31 06:44:18 -0700

    update header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: pet identifier
  */
-typedef struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 

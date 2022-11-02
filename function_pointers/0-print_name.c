03cdae96cd3ff9df735588833cf89389891b9814
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-02 08:26:58 -0700

    typo fix

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "functions_pointers.h"
+#include "function_pointers.h"
 /**
  * print_name - a function
  * @name: parameter to be printed

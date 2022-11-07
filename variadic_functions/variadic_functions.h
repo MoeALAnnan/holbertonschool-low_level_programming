6aeac331482c571748ac27b58bc687935684dc31
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-07 06:49:15 -0800

    update header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdarg.h>
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list ap);
+
+} op_f;
+void print_int(va_list ap);
+void print_char(va_list ap);
+void print_str(va_list ap);
+void print_flt(va_list ap);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif

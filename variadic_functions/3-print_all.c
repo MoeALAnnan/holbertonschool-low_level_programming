086410f8b6f3d9dafbac9dd17eedaa6b33efef5e
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-07 06:50:26 -0800

    check errors

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_int - print integers
+ * @ap: variable list
+ * Return: void
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+/**
+ * print_char - print char
+ * @ap: variable list
+ * Return: void
+ */
+void print_char(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+/**
+ * print_str - print strings
+ * @ap: variable list
+ * Return: void
+ */
+void print_str(va_list ap)
+{
+	if (ap == NULL)
+	{
+		printf("(nil)");
+	}
+
+	printf("%s", va_arg(ap, char *));
+}
+/**
+ * print_flt - print float
+ * @ap: variable list
+ * Return: void
+ */
+void print_flt(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+/**
+ * print_all - print float
+ * @format: the format scope
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	op_f ops[] = {
+
+		{"i", print_int},
+		{"c", print_char},
+		{"s", print_str},
+		{"f", print_flt},
+		{"\0", NULL}
+	};
+	va_list ap;
+	int i;
+	int j;
+
+
+	i = 0;
+
+	va_start(ap, format);
+	while (format[i] != '\0')
+
+	{
+		j = 0;
+		while (j < 4)
+
+		{
+			if (*ops[j].op == format[i])
+		{
+				ops[i].f(ap);
+				printf(",");
+		}
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+
+
+}

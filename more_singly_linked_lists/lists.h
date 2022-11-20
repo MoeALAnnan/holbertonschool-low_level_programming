674bbcb9daefac9d9782f717cc74a1ba1355b22c
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-20 12:43:20 -0800

    fixing multiple errors in header file

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #ifndef LISTS_H
 #define LISTS_H
 /**
@@ -12,7 +14,8 @@ typedef struct listint_s
 {
     int n;
     struct listint_s *next;
-} listint_t
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);

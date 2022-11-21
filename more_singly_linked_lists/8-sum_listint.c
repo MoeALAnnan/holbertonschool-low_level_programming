65f561aec61484f5748a9967d6c139de891f5b5a
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-21 07:51:09 -0800

    a function that adds data in all the nodes

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * sum_listint - function
+ * @head: head of single linked list
+ * Description: a function that adds data in a list
+ * Return: int
+ */
+int sum_listint(listint_t *head)
+{
+	int n;
+	listint_t *temp;
+
+	temp = head;
+	n = temp->n;
+	if (head == NULL)
+		return (0);
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		n = n + temp->n;
+	}
+	return (n);
+}

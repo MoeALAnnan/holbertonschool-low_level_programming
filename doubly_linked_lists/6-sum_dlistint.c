63c3b58779a7b6df28963c73ce21da593308de0e
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-24 07:55:09 -0800

    summing list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * sum_listint - function
+ * @head: head of single linked list
+ * Description: a function that adds data in a list
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int n;
+	dlistint_t *temp;
+
+	/**
+	 * temp = head;
+	 *n = temp->n;
+	 */
+	if (head == NULL)
+	{
+		return (0);
+	}
+	temp = head;
+	n = temp->n;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		n = n + temp->n;
+	}
+	return (n);
+}

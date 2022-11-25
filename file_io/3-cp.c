b9b84488dda740ba00508e6847183aca673d7c81
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-25 07:15:27 -0800

    fixing error statement

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	fdsource = open(argv[1], O_RDONLY);
 	if (fdsource == -1)
 	{
-		dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 000664);
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		wr = write(fddest, buffer, size);
 		if (wr == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}

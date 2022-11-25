c9780dc7d5ff897e6703af2a533fde1d3dddd0ed
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-25 08:13:44 -0800

    generate index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - a function.
+ * @key: key used to generate index
+ * @size: size of the hash table
+ *
+ * Return: index of a key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key || size <= 0)
+		return (0);
+	return (hash_djb2(key) % size * 2);
+}
+

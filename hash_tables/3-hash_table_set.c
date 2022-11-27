77aa0453a466438c33d8f102f0421c7b5ca8053c
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-27 06:42:18 -0800

    hash tables

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+unsigned long int ht_length(hash_table_t *table)
+{
+	return (table->size);
+}
+hash_node_t *ht_pair(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(node));
+
+	node->key = malloc(sizeof(key) + 1);
+	node->value = malloc(sizeof(value) + 1);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	node->next = NULL;
+
+	return (node);
+}
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i = ht_length(ht);
+	unsigned long int index = key_index((const unsigned char *)key, i);
+	hash_node_t *node = ht->array[index];
+	hash_node_t *temp;
+
+	if (node == NULL)
+	{
+		ht->array[index] = ht_pair(key, value);
+	}
+	return (0);
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = malloc(strlen(value) + 1);
+			strcpy(node->value, value);
+			return (0);
+		}
+		temp = node;
+		node = temp->next;
+
+	}
+	temp->next = ht_pair(key, value);
+}

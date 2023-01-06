0fc37fcf93da122da50491e7b07116cf662312f1
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-06 13:22:09 -0800

    fixing infinite loop

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -8,7 +8,7 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l, r, mid;
+	int l, r, mid;
 
 	l = 0;
 	r = size - 1;
@@ -19,13 +19,15 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (mid = l; mid < r; mid++)
 			printf("%d, ", array[mid]);
+
 		printf("%d\n", array[mid]);
-		mid = l + (r - 1) / 2;
+
+		mid = l + (r - l) / 2;
+
 		if (value == array[mid])
 			return (mid);
 
 		else if (value < array[mid])
-
 			r = mid - 1;
 		else
 			l = mid + 1;
